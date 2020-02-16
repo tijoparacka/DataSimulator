d5e2c0325b1ac64c45eb177ae70c61e9d146bef2
tijoparacka <tijoparacka@gmail.com>
2020-02-16 15:37:49 -0800

    This will generate entries for the configured cardinality for the type: "FIXED" under the path mentioned in file:<path> parameter in the config.properties

---
+++ simulator/src/main/java/com/tijo/CardinalityGenerator.java
@@ -0,0 +1,64 @@
+package com.tijo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tijo.config.ConfigUtil;
+import com.tijo.streaming.Util;
+import com.tijo.streaming.impl.domain.generic.GenericEventGenerator;
+import com.tijo.streaming.impl.domain.generic.MetaData;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CardinalityGenerator
+{
+  private static MetaData[] metaDatas;
+
+  public static void main(String[] args) throws Exception
+  {
+    if (args != null && args.length != 1) {
+      System.out.println(" Please provide a config file .");
+      System.exit(0);
+    }
+    ConfigUtil conf = new ConfigUtil(args[0]);
+    String metaDataJson = conf.getConfig("sim.generic.metadata");
+    ObjectMapper objectMapper = new ObjectMapper();
+    metaDatas = objectMapper.readValue(metaDataJson, MetaData[].class);
+    for (MetaData metaData : metaDatas) {
+      try {
+        if (metaData.getType().equalsIgnoreCase(GenericEventGenerator.FIXED)) {
+          generateRandomData(
+              metaData.getFile(),
+              metaData.getDimension(),
+              metaData.getLimit(),
+              metaData.getCardinality()
+          );
+
+        }
+      }
+      catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  private static void generateRandomData(String file, String dimension, Double limit, int cardinality)
+      throws IOException
+  {
+
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0; i < cardinality; i++) {
+      sb.append(Util.randomAlphaNumeric(limit.intValue()) ).append("\n");
+    }
+    Path path = Paths.get(file);
+    Files.write(path,sb.toString().getBytes(StandardCharsets.UTF_8));
+    System.out.println("Written Random data to "+file);
+  }
+}
