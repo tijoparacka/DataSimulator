b8b7ab8e69c9dfa08d5d1fd9c3a62434d6de30e7
tijoparacka <tijoparacka@gmail.com>
2020-03-07 18:01:23 +0530

    For Generic Event Generator

--- distribution/bin/genCardinalityFile.sh
+++ distribution/bin/genCardinalityFile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 bin_dir=`dirname $0`
-java  -cp $bin_dir/../lib/simulator-1.1.0-SNAPSHOT-jar-with-dependencies.jar com.tijo.CardinalityGenerator $bin_dir/conf/config.properties
+java  -cp $bin_dir/../lib/simulator-1.1.0-SNAPSHOT-jar-with-dependencies.jar com.tijo.CardinalityGenerator $bin_dir/../conf/config.properties
 
