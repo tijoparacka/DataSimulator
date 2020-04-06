4192299c8f97957d6afe67ec6c4fa0a30fa19938
tijoparacka <tijoparacka@gmail.com>
2020-04-06 15:00:13 +0530

    kinesis sim start script

---
+++ distribution/bin/start_generic_kinesis.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+bin_dir=`dirname $0`
+$bin_dir/start.sh 1 -1 com.tijo.streaming.impl.domain.generic.GenericEventGenerator  com.tijo.streaming.impl.collectors.KinesisEventCollector  json "$@"
