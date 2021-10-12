76f6cf80280358b0eebe131061f09bf53af2192c
tijoparacka <AN11**na>
2021-10-12 10:15:29 +0530

    Feature push data with a  key col to kafka . Useful if the data is generated for kafka lookup purpose.

--- simulator/src/main/java/com/tijo/streaming/impl/collectors/UnsecuredKafkaEventCollector.java
+++ simulator/src/main/java/com/tijo/streaming/impl/collectors/UnsecuredKafkaEventCollector.java
@@ -11,6 +11,7 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
+import java.lang.reflect.Method;
 import java.util.Properties;
 
 public class UnsecuredKafkaEventCollector extends AbstractEventCollector {
@@ -18,7 +19,10 @@ public class UnsecuredKafkaEventCollector extends AbstractEventCollector {
     private String bootstrapServer;
     private Properties props;
     private  int maxLoggingRows;
+    private String  keyColumn ;
     private long startTime =System.currentTimeMillis();
+    private Class eventClass ;
+    private Method method;
     private org.apache.kafka.clients.producer.Producer<String, String> producer;
 
     public UnsecuredKafkaEventCollector() {
@@ -37,6 +41,12 @@ public class UnsecuredKafkaEventCollector extends AbstractEventCollector {
                     "org.apache.kafka.common.serialization.StringSerializer");
             producer = new KafkaProducer<String, String>(props);
             String rows =config.getConfig("sim.logging.rowCount");
+            keyColumn = config.getConfig("sim.kafka.keyColumn");
+            if(keyColumn !=null && keyColumn.trim().length() >0){
+                eventClass = Class.forName(config.getConfig("sim.generic.eventClass"));
+                method = eventClass.getDeclaredMethod("get" + keyColumn.substring(0, 1).toUpperCase()
+                                                        + keyColumn.substring(1),null);
+            }
             if(rows != null &&  rows.trim().length() == 0 ){
                 maxLoggingRows = 5000000;
                 logger.info("Max Rows is not configured. Defaulting to 5,000,000");
@@ -56,19 +66,21 @@ public class UnsecuredKafkaEventCollector extends AbstractEventCollector {
 
     @Override
     public void onReceive(Object message) throws Exception {
-//        logger.debug(message);
+        logger.debug(message);
         if (message instanceof DumpStats) {
             logger.info("Processed " + numberOfEventsProcessed + " events");
             return;
         }else if( message instanceof Event) {
             try {
-                producer.send(new ProducerRecord<String, String>(topicName,
-                                                                 (writer.writeValueAsString ((Event) message)).replace("\n", "")));
+                String data = writer.writeValueAsString ((Event) message).replace("\n", "");
+                method.setAccessible(true);
+                String result = (String)method.invoke(message);
+                producer.send(new ProducerRecord<String, String>(topicName,result,data ));
                 numberOfEventsProcessed++;
                 if(numberOfEventsProcessed % maxLoggingRows == 0){
                     logger.info("Processed " + numberOfEventsProcessed + " events");
-                 //   logger.info("Number of events processed per sec  = " + (maxLoggingRows /(  (System.currentTimeMillis() - startTime ) * 1000))) ;
-                 //   startTime = System.currentTimeMillis();
+                    logger.info("Number of events processed per sec  = " + (maxLoggingRows /(  (System.currentTimeMillis() - startTime ) * 1000))) ;
+                    startTime = System.currentTimeMillis();
                 }
             }
             catch (Exception e) {
