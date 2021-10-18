de2706de4214174531b5822d891bfbddcd2a54fe
tijoparacka <AN11**na>
2021-10-18 16:19:54 +0530

    cleanup

--- distribution/bin/start.sh
+++ distribution/bin/start.sh
@@ -2,7 +2,7 @@
 bin_dir=`dirname $0`
 #jarpath = `$bin_dir/../lib/simulator-1.1.0-SNAPSHOT-jar-with-dependencies.jar`
 jarpath=".:$bin_dir/../lib/"
-confpath=".:$bin_dir/../conf/"
+#confpath=".:$bin_dir/../conf/"
 for file in $bin_dir/../lib/*
 do
   jarpath=$jarpath:$file
@@ -14,7 +14,7 @@ java -server -Xmx2048m \
 -XX:+ExitOnOutOfMemoryError \
 -XX:+PrintGC \
 -Xloggc:gc.log -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=5 -XX:GCLogFileSize=2M \
--XX:HeapDumpPath=/tmp/druid-heap.hprof -cp "$confpath/log4j.properties":""$jarpath" com.tijo.DataSimulator $bin_dir/../ "$@" 2>$1 &
+-XX:HeapDumpPath=/tmp/druid-heap.hprof -cp "$jarpath" com.tijo.DataSimulator $bin_dir/../ "$@" 2>$1 &
 _pid=$!
 echo "$_pid" > $bin_dir/../app.pid
 
