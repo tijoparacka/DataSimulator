831e04ad224e07e17b87a7dd1d4423ad9e955753
tijoparacka <AN11**na>
2021-11-10 14:10:10 +0530

    Start Script for S3 collector

---
+++ distribution/bin/start_generic_s3.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+bin_dir=`dirname $0`
+$bin_dir/start.sh 1 -1 com.tijo.streaming.impl.domain.generic.GenericEventGenerator  com.tijo.streaming.impl.collectors.S3FileEventCollector json "$@"
+
